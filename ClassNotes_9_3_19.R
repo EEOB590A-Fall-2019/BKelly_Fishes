ec6007046c414db805a69c96b3f1ff07e6834c34
bbkelly95 <bbkelly@iastate.edu>
2019-09-03 15:00:22 -0500

    a couple notes from 9/3/19

---
+++ ClassNotes_9_3_19.R
@@ -0,0 +1,7 @@
+# Introduction to RStudio - Class Notes 09/03/2019
+
+#what/where is my working directory? Let's Check!
+getwd()
+#Same PLace as where your Project File (BKelly_Fishes) is housed
+
+#transfer over to course materials notes 
