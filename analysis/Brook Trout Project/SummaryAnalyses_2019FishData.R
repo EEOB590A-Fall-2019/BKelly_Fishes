d3d40d5d4e1a743694dc425a0ea4c0ac9dfe5469
bbkelly95 <bbkelly@iastate.edu>
2019-11-18 11:40:56 -0600

    data exploration for 2019 DNR report

---
+++ analysis/Brook Trout Project/SummaryAnalyses_2019FishData.R
@@ -0,0 +1,8 @@
+##-----------------------------------##
+##    Fish data Exploration 2019     ##
+## we have fish data for all sites 
+## sampled in 2019, in wadable 
+## streams. We want to summarize the 
+## data for use in reports and 
+## manuscripts. 
+##-----------------------------------##
