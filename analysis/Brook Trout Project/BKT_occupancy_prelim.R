ca268ca42fa7672f4cb7b68710cd47bb3f273c02
bbkelly95 <bbkelly@iastate.edu>
2019-12-03 08:53:26 -0600

    an unworldly amount of MARK files

--- analysis/Brook Trout Project/BKT_occupancy_prelim.R
+++ analysis/Brook Trout Project/BKT_occupancy_prelim.R
@@ -20,6 +20,11 @@ library(RMark)
 library(tidyselect)
 library(tidyverse)
 
+##Occupancy example
+?weta
+
+
+
 ##--------------------------------------------------------------------------------------------------------------------------------##
 #read in data, rearrange and change some labels to work with grouping ("freq"), and time-varying covariates ("Effort1 --> Effort3")
 brook <- read_csv("Data/Thesis/Tidy/BKT_occDF_RMARK.csv", col_names = T)
@@ -28,10 +33,12 @@ brook$freq <- 1
 brook <- brook[,c(1,74,2:73)]
 names(brook)
 
-brook <- brook %>%
-  rename(effort1 = t1_eff, effort2 = t2_eff, effort3 = t3_eff, under1 = t1_under, under2=t2_under, under3=t3_under, maxdep1 = t1_maxdep,
-         maxdep2 = t2_maxdep, maxdep3 = t3_maxdep, avdep1 = t1_avdep, avdep2 = t2_avdep, avdep3 = t3_avdep)%>%
-  select(-t1_avwid, -t2_avwid, -t3_avwid, -t1_LWD, -t2_LWD, -t3_LWD)
+brook2 <- brook %>%
+  rename(effort1 = t1_eff, effort2 = t2_eff, effort3 = t3_eff, undr1 = t1_under, undr2=t2_under, undr3=t3_under, mxdep1 = t1_maxdep,
+         mxdep2 = t2_maxdep, mxdep3 = t3_maxdep, mxflow1=t1_maxflow, mxflow2=t2_maxflow, mxflow3=t3_maxflow,
+         lwd1=t1_LWD, lwd2=t2_LWD, lwd3=t3_LWD)%>%
+  select(-temp,-avwid,-sdwid,-t1_avwid, -t2_avwid, -t3_avwid, -t1_avdep, -t2_avdep, -t3_avdep,
+         -mFlow, -sdFlow, -t1_flow, -t2_flow, -t3_flow, -pctEmb1, -boulder, -Avbnka, -pctShadeSD)
 
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~##
@@ -63,7 +70,8 @@ brook <- brook %>%
 #Process Data
 ?process.data
 ?make.design.data
-brook.process = process.data(brook, model="Occupancy")
+
+brook.process = process.data(brook2, model="Occupancy", groups = "freq")
 bkt.ddl = make.design.data(brook.process)
 
 
@@ -75,7 +83,22 @@ bkt.ddl = make.design.data(brook.process)
 run.occ=function()
 {
   #~~~~~~~~~~~~~ Model List ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  #~~~~~~~~~~~~~ Occupancy - single covariate ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #~~~~~~~~~~~ Detection Probability - single covariate ~~~~~~~~~~~~
+  p.Dot = list(formula= ~1)
+  p.effort = list(formula= ~effsec) 
+  p.tv.effort = list(formula = ~effort)
+  p.maxdep = list(formula = ~maxdep)
+  p.tv.mxdep = list(formula = ~mxdep)
+  p.maxflow = list(formula = ~maxflow)
+  p.tv.mxflow = list(formula = ~mxflow)
+  p.LWD = list(formula = ~LWD)
+  p.tv.lwd = list(formula = ~lwd)
+  p.under = list(formula = ~under)
+  p.tv.under = list(formula = ~undr)
+  
+  #~~~~~~~~~~~~~ Occupancy - single covariate ~~~~~~~~~~~~~~~~~~~~~~
+  #---Local (instream and riparian) Scale Variables---
+  #Temperature covariates
   Psi.Dot        = list(formula=~1)
   Psi.avgT       = list(formula=~avgT)
   Psi.MAXT  = list(formula=~MAXT)
@@ -83,42 +106,79 @@ run.occ=function()
   Psi.RNGT = list(formula=~RNGT)
   Psi.pctex21 = list(formula=~pctex21)
   
-  #Psi.depth = list(formula=~avdep)
-  #Psi.maxdep = list(formula=~maxdep)
-  #Psi.WD = list(formula=~WidDep)
+  #Stream Dimensions
+  Psi.depth = list(formula=~avdep)
+  Psi.WD = list(formula=~WidDep)
   
-  #Psi.slow = list(formula=~pctslow)
-  #Psi.MacroProp = list(formula=~machabprop)
+  #Macrohabitat
+  Psi.slow = list(formula=~pctslow)
+  Psi.MacroProp = list(formula=~machabprop)
   
-  #Psi.rock = list(formula=~pctrock)
-  #Psi.gravel = list(formula=~pctgrvl)
+  #Substrate
+  Psi.rock = list(formula=~pctrock)
   
-  #Psi.under = list(formula=~under)
-  #Psi.boulder = list(formula=~boulder)
-  #Psi.pool = list(formula=~depool)
+  #Instream Complexity
+  Psi.under = list(formula=~under)
+  Psi.pool = list(formula=~depool)
   
-  #Psi.Vertical = list(formula=~pctVertbnk)
-  #Psi.bare = list(formula=~pctBrBnk)
+  #Bank Condition
+  Psi.Vertical = list(formula=~pctVertbnk)
+  Psi.bare = list(formula=~pctBrBnk)
+  Psi.shade = list(formula=~pctShade)
   
-  #Psi.shade = list(formula=~pctShade)
+  #Fish Covariates -- only interested in trout density
+  Psi.brt = list(formula=~BRT_100m)
   
-  #Psi.brt = list(formula=~BRT_100m)
+  #---Catchment Scale Variables---
+  #Landcover
+  Psi.HAiFLS.alt = list(formula=~HAiFLS_alt)
+  Psi.HAiFLS.nat = list(formula=~HAiFLS_nat)
   
-  #Psi.cat = list(formula=~CatArea_km2)
-  #Psi.HAiFLS.alt = list(formula=~HAiFLS_alt)
-  #Psi.HAiFLS.for = list(formula=~HAiFLS_for)
-  #~~~~~~~~~~~ Detection Probability ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  p.Dot     = list(formula=~1)
-  p.effort    = list(formula=~effsec) 
-  p.tv.effort = list(formula = ~ effort)
- 
+  #Catchment Attributes
+  Psi.cat = list(formula = ~CatArea_km2)
+  
+  #Stream Segment Attributes
+  Psi.slope = list(formula = ~pctSlope)
+  Psi.sinu = list(formula = ~Sinuosity)
+  
   #~~~~~~~~~~~~ model list & wrapper ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   cml=create.model.list("Occupancy")
-  results=mark.wrapper(cml, data=brook.process, ddl=bkt.ddl, adjust=F, output=F, threads=-1)
+  results=mark.wrapper(cml, data=brook.process, ddl=bkt.ddl)
   return(results)
 }
 
-occ.results=run.occ()
-occ.results.initial <- occ.results
+bkt.models1 = run.occ()
+
+
+##Examine model list and look at model comparisons
+bkt.models1
+##use AIC instead of AICc
+AIC.table = bkt.models1
+AIC.table$model.table = model.table(bkt.models1, use.AIC = T)
+AIC.table
+
+#look at summary of top model
+names(bkt.models1)
+round1.top <- bkt.models1$p.effort.Psi.pctex21
+summary(round1.top, showall = F)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
