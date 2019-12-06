66eeb6191f06c4db8fb19eaf5038f97b75115d6f
bbkelly95 <bbkelly@iastate.edu>
2019-12-06 11:47:05 -0600

    BKT occu analysis

--- analysis/Brook Trout Project/RMark/BKT_.R
+++ analysis/Brook Trout Project/RMark/BKT_.R
@@ -20,10 +20,10 @@ library(RMark)
 library(tidyverse)
 library(skimr)
 library(psycho)
+library(corrplot)
 
 ##Occupancy example
 #?weta
-
 
 
 ##--------------------------------------------------------------------------------------------------------------------------------##
@@ -51,6 +51,48 @@ skim(x)
 
 brook3 <- brook2 
 brook3[,4:6]=x
+
+#correlation test
+c <- cor(brook3[,4:14])
+head(round(c,2)) 
+
+#round down
+cround <- round(c,3)
+
+#visualize these correlations
+corrplot(c, method = "number")
+
+# mat : is a matrix of data
+# ... : further arguments to pass to the native R cor.test function
+cor.mtest <- function(mat, ...) {
+  mat <- as.matrix(c)
+  n <- ncol(mat)
+  p.mat<- matrix(NA, n, n)
+  diag(p.mat) <- 0
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      tmp <- cor.test(mat[, i], mat[, j], ...)
+      p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+    }
+  }
+  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+  p.mat
+}
+# matrix of the p-value of the correlation
+p.mat <- cor.mtest(brook3[,4:14])
+
+#correlogram
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+corrplot(c, method="color", col=col(200),  
+         type="upper", order="hclust", 
+         addCoef.col = "black", # Add coefficient of correlation
+         tl.col="black", tl.srt=45, #Text label color and rotation
+         # Combine with significance
+         p.mat = p.mat, sig.level = 0.01, insig = "blank", 
+         # hide correlation coefficient on the principal diagonal
+         diag=FALSE 
+)
+
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 #### Variables of interest and Data Dictionary ####
