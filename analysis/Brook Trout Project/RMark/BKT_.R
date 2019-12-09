c7482da77ff08896f888c96d767ff7b389edfb7d
bbkelly95 <bbkelly@iastate.edu>
2019-12-09 17:37:56 -0600

    bkt occu

--- analysis/Brook Trout Project/RMark/BKT_.R
+++ analysis/Brook Trout Project/RMark/BKT_.R
@@ -1,5 +1,5 @@
 ##########################################################################################################################
-#                                       Preliminary BKT Occupancy Analysis in RMark -- Heirachical
+#                  Brook Trout Occupancy Analysis in RMark -- Information Criteria Model Selection
 ##########################################################################################################################
 
 # We have data on Brook Trout occurence across 138 unique sites (stream segments of 1st-4th order), 
@@ -21,6 +21,7 @@ library(tidyverse)
 library(skimr)
 library(psycho)
 library(corrplot)
+library(AICcmodavg)
 
 ##Occupancy example
 #?weta
@@ -29,21 +30,25 @@ library(corrplot)
 ##--------------------------------------------------------------------------------------------------------------------------------##
 #read in data, rearrange and change some labels to work with grouping ("freq"), and time-varying covariates ("Effort1 --> Effort3")
 brook <- read_csv("Data/Thesis/Tidy/BKT_Occu_File.csv", col_names = T)
-
+brook.df <- as.data.frame(brook)
 #examine
-skim(brook)
+skim(brook.df)
 
-#standardize covariates to have center 0 --- should we do this? (subtract mean and divide by sd)
-x <- brook %>%
+#---------------------------------------------#
+#standardize covariates to have center 0 --- Only effort variables (all high and 3-4 digits) (subtract mean and divide by sd)
+#---------------------------------------------#
+x <- brook.df %>%
   select(4:6) %>%
   psycho::standardize()
 summary(x)
 skim(x)
 
-brook2 <- brook 
+brook2 <- brook.df 
 brook2[,4:6]=x
 
+#----------------#
 #correlation test
+#----------------#
 c <- cor(brook2[,4:18])
 head(round(c,2)) 
 
@@ -84,14 +89,31 @@ corrplot(c, method="color", col=col(200),
          diag=FALSE 
 )
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+## Colinear variables to not include in the same model (> 0.6):
+# Strong #
+#HAiFLS_for and HAiFLS_alt (-0.96)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Weak - user discretion #
+#------------------------#
+#Area_km2 and effort (0.43)
+#pctShade and HAiFLS_for (0.4)
+#pctShade and HAiFLS_alt (-0.37)
+#HAiFLS_for and Cross_Cat (-0.36)
+#HAiFLS_alt and Cross_Cat (0.35)
+#pctShade and Cross_Cat (-0.25)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 #### Variables of interest and Data Dictionary ####
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+#-----#
 #Detection Probability: probability of detecting Brook Trout at a site
+#-----#
 #> time varying effort (+) "amount (seconds) of time spent sampling (aka shocking)"
 
+#-----#
 #Occupancy Probability: probability of Brook Trout occurrence among sites
+#-----#
 # Local Scale: instream and immediate riparian area
 #> pctex21 (-) "percentage of summer temperature observations that exceed 21 degrees C"
 #> pctpool (+) "percentage of pool habitats"
@@ -99,11 +121,16 @@ corrplot(c, method="color", col=col(200),
 #> pctBrBnk (-) "percentage of bank that is bare soil"
 #> pctShade (+) "average percent canopy cover"
 #> BRT_100m (-) "Brown Trout Catch-Per 100m of stream sampled"
+# Catchment Scale: within the upstream land area that drains to the outlet of the sampled segment
+#> HAiFLS_for (+) "Hydrologically Active inserve flow length to the stream of forest LULC"
+#> Area_km2 (-) "Catchment Area"
+#> AvgSlope (+) "Mean Slope of the catchment"
+#> EFac_Cat (-) "Environmental Facility density of upstream catchment (count/Area_km2)"
+#> Cross_Cat (-) "Road Crossing density of upstream catchment (count/Area_km2)"
 ##########################################################################################
 getwd()
 #set wd to scratch folder because MARK outputs an insane amount of files
 setwd("C:/Users/bbkelly/Documents/Brook Trout_Brett/BKelly_Fishes_GithubRepos/Analysis/Brook Trout Project/RMark/BrookTrout_ScratchFolder") #because MARK loves output files
-
 
 #Process Data
 #?process.data
@@ -118,10 +145,10 @@ bkt.ddl = make.design.data(brook.process)
 ############################################################
 
 ###~~~~~~~~~~~~~##
-#### Round 1 ####
+####  Local   ####
 ##~~~~~~~~~~~~~##
 
-run.occ=function()
+run.occ.loc=function()
 {
   #~~~~~~~~~~~~~ Model List ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #~~~~~~~~~~~ Detection Probability - null model ~~~~~~~~~~~~
@@ -139,92 +166,177 @@ run.occ=function()
   Psi.pct21_pool = list(formula = ~pctex21 + pctpool) 
   Psi.pct21_rock = list(formula = ~pctex21 + pctrock) 
   Psi.pct21_brt = list(formula = ~pctex21 + BRT_100m)
-  #indirect effects - hypothesized smaller effects (features of high quality stream habitats (for trout))
+  #indirect effects - hypothesized smaller effects (features of high quality stream habitats (for Brook Trout))
+  Psi.indEff = list(formula = ~pctBrBnk+pctShade+HAiFLS_for)
   Psi.BBnk_shade = list(formula = ~pctBrBnk+pctShade)
+  Psi.BBnk_for = list(formula = ~pctBrBnk+HAiFLS_for)
+  Psi.shade_for = list(formula = ~pctShade+HAiFLS_for)
   #combination of effects
-  Psi.global = list(formula = ~pctex21+pctpool+pctrock+LWD+pctBrBnk+BRT_100m)
-  Psi.global2 = list(formula = ~pctex21+pctpool+pctrock+pctBrBnk+pctShade+BRT_100m)
-  Psi.mixed1 = list(formula = ~pctex21+pctpool+pctrock+BRT_100m+LWD)
-  Psi.mixed2 = list(formula = ~pctex21+pctpool+pctrock+BRT_100m+pctBrBnk)
-  Psi.mixed3 = list(formula = ~pctex21+pctpool+pctrock+BRT_100m+pctShade)
-  Psi.mixed4 = list(formula = ~pctex21+pctpool+pctrock+LWD+pctBrBnk)
-  Psi.mixed5 = list(formula = ~pctex21+pctpool+pctrock+pctShade+pctBrBnk)
-  Psi.mixed6 = list(formula = ~pctex21+pctpool+BRT_100m+LWD+pctBrBnk)
-  Psi.mixed7 = list(formula = ~pctex21+pctpool+BRT_100m+pctShade+pctBrBnk)
-  Psi.mixed8 = list(formula = ~pctex21+pctrock+BRT_100m+LWD+pctBrBnk)
-  Psi.mixed9 = list(formula = ~pctex21+pctrock+BRT_100m+pctShade+pctBrBnk)
-  Psi.mixed10 = list(formula = ~pctpool+pctrock+BRT_100m+LWD+pctBrBnk)
-  Psi.mixed11 = list(formula = ~pctpool+pctrock+BRT_100m+pctShade+pctBrBnk)
-  Psi.mixed12 = list(formula = ~pctex21+pctpool+LWD+pctBrBnk)
-  Psi.mixed13 = list(formula = ~pctex21+pctpool+pctShade+pctBrBnk)
-  Psi.mixed14 = list(formula = ~pctex21+pctpool+LWD+pctBrBnk)
-  Psi.mixed15 = list(formula = ~pctex21+pctpool+pctShade+pctBrBnk)
-  Psi.mixed16 = list(formula = ~pctex21+BRT_100m+LWD+pctBrBnk)
-  Psi.mixed17 = list(formula = ~pctex21+BRT_100m+pctShade+pctBrBnk)
-  Psi.mixed18 = list(formula = ~pctrock+BRT_100m+LWD+pctBrBnk)
-  Psi.mixed19 = list(formula = ~pctrock+BRT_100m+pctShade+pctBrBnk)
-  Psi.mixed20 = list(formula = ~pctpool+pctrock+LWD+pctBrBnk)
-  Psi.mixed21 = list(formula = ~pctpool+pctrock+pctShade+pctBrBnk)
-  Psi.mixed22 = list(formula = ~pctex21+pctrock+LWD+pctBrBnk)
-  Psi.mixed23 = list(formula = ~pctex21+pctrock+pctShade+pctBrBnk)
-  Psi.mixed24 = list(formula = ~pctpool+BRT_100m+LWD+pctBrBnk)
-  Psi.mixed25 = list(formula = ~pctpool+BRT_100m+pctShade+pctBrBnk)
-  Psi.mixed26 = list(formula = ~pctex21+LWD+pctBrBnk)
-  Psi.mixed27 = list(formula = ~pctex21+pctShade+pctBrBnk)
-  Psi.mixed28 = list(formula = ~pctpool+LWD+pctBrBnk)
-  Psi.mixed29 = list(formula = ~pctpool+pctShade+pctBrBnk)
-  Psi.mixed30 = list(formula = ~pctrock+LWD+pctBrBnk)
-  Psi.mixed31 = list(formula = ~pctrock+pctShade+pctBrBnk)
-  Psi.mixed32 = list(formula = ~BRT_100m+LWD+pctBrBnk)
-  Psi.mixed33 = list(formula = ~BRT_100m+pctShade+pctBrBnk)
-  Psi.pct21_lwd = list(formula = ~pctex21 + LWD) 
+  #all covariates
+  Psi.global = list(formula = ~pctex21+pctpool+pctrock+BRT_100m+pctBrBnk+pctShade+HAiFLS_for)
+  #6 covariates
+  Psi.mixed1 = list(formula = ~pctex21+pctpool+pctrock+BRT_100m+pctBrBnk+pctShade)
+  Psi.mixed2 = list(formula = ~pctex21+pctpool+pctrock+BRT_100m+pctBrBnk+HAiFLS_for)
+  Psi.mixed3 = list(formula = ~pctex21+pctpool+pctrock+BRT_100m+pctShade+HAiFLS_for)
+  Psi.mixed4 = list(formula = ~pctex21+pctpool+pctrock+pctBrBnk+pctShade+HAiFLS_for)
+  Psi.mixed5 = list(formula = ~pctex21+pctpool+BRT_100m+pctBrBnk+pctShade+HAiFLS_for)
+  Psi.mixed6 = list(formula = ~pctex21+pctrock+BRT_100m+pctBrBnk+pctShade+HAiFLS_for)
+  Psi.mixed7 = list(formula = ~pctpool+pctrock+BRT_100m+pctBrBnk+pctShade+HAiFLS_for)
+  #5 covariates
+  Psi.mixed8 = list(formula = ~pctex21+pctpool+pctrock+BRT_100m+pctBrBnk)
+  Psi.mixed9 = list(formula = ~pctex21+pctpool+pctrock+BRT_100m+pctShade)
+  Psi.mixed10 = list(formula = ~pctex21+pctpool+pctrock+pctShade+pctBrBnk)
+  Psi.mixed11 = list(formula = ~pctex21+pctpool+BRT_100m+pctShade+pctBrBnk)
+  Psi.mixed12 = list(formula = ~pctex21+pctrock+BRT_100m+pctShade+pctBrBnk)
+  Psi.mixed13 = list(formula = ~pctpool+pctrock+BRT_100m+pctShade+pctBrBnk)
+  Psi.mixed14 = list(formula = ~pctex21+pctpool+pctrock+BRT_100m+HAiFLS_for)
+  Psi.mixed15 = list(formula = ~pctex21+pctpool+pctrock+pctShade+HAiFLS_for)
+  Psi.mixed16 = list(formula = ~pctex21+pctpool+pctShade+pctBrBnk+HAiFLS_for)
+  Psi.mixed17 = list(formula = ~pctex21+BRT_100m+pctShade+pctBrBnk+HAiFLS_for)
+  Psi.mixed18 = list(formula = ~pctrock+BRT_100m+pctShade+pctBrBnk+HAiFLS_for)
+  Psi.mixed19 = list(formula = ~pctpool+pctrock+BRT_100m+pctBrBnk+HAiFLS_for)
+  Psi.mixed20 = list(formula = ~pctex21+pctrock+BRT_100m+pctShade+HAiFLS_for)
+  Psi.mixed21 = list(formula = ~pctpool+pctrock+BRT_100m+pctBrBnk+HAiFLS_for)
+  Psi.mixed22 = list(formula = ~pctex21+pctpool+pctrock+pctBrBnk+HAiFLS_for)
+  Psi.mixed23 = list(formula = ~pctex21+pctpool+BRT_100m+pctShade+HAiFLS_for)
+  Psi.mixed24 = list(formula = ~pctex21+pctrock+pctBrBnk+pctShade+HAiFLS_for)
+  Psi.mixed25 = list(formula = ~pctpool+BRT_100m+pctBrBnk+pctShade+HAiFLS_for)
+  Psi.mixed26 = list(formula = ~pctex21+pctpool+BRT_100m+pctBrBnk+HAiFLS_for)
+  Psi.mixed27 = list(formula = ~pctex21+pctrock+BRT_100m+pctBrBnk+HAiFLS_for)
+  Psi.mixed28 = list(formula = ~pctpool+pctrock+pctBrBnk+pctShade+HAiFLS_for)
+  Psi.mixed29 = list(formula = ~pctpool+pctrock+BRT_100m+pctShade+HAiFLS_for)
+  #4 covariates
+  #~pctex21 + pctpool + pctrock + BRT_100m + pctBrBnk + pctShade + HAiFLS_for
+  Psi.mixed30 = list(formula = ~pctex21+pctpool+pctShade+pctBrBnk)
+  Psi.mixed31 = list(formula = ~pctex21+pctrock+pctShade+pctBrBnk)
+  Psi.mixed32 = list(formula = ~pctex21+BRT_100m+pctShade+pctBrBnk)
+  Psi.mixed33 = list(formula = ~pctrock+BRT_100m+pctShade+pctBrBnk)
+  Psi.mixed34 = list(formula = ~pctpool+pctrock+pctShade+pctBrBnk)
+  Psi.mixed35 = list(formula = ~pctpool+BRT_100m+pctShade+pctBrBnk)
+  Psi.mixed35 = list(formula = ~pctex21+pctpool+pctrock+HAiFLS_for)
+  Psi.mixed35 = list(formula = ~pctex21+pctpool+pctShade+HAiFLS_for)
+  Psi.mixed35 = list(formula = ~pctex21+pctBrBnk+pctShade+HAiFLS_for)
+  Psi.mixed35 = list(formula = ~BRT_100m+pctBrBnk+pctShade+HAiFLS_for)
+  Psi.mixed35 = list(formula = ~pctex21+pctpool+pctrock+HAiFLS_for)
+  #3 covariates
+  Psi.mixed13 = list(formula = ~pctex21+pctShade+pctBrBnk)
+  Psi.mixed14 = list(formula = ~pctpool+pctShade+pctBrBnk)
+  Psi.mixed15 = list(formula = ~pctrock+pctShade+pctBrBnk)
+  Psi.mixed16 = list(formula = ~BRT_100m+pctShade+pctBrBnk)
+  #2 covariates
   Psi.pct21_bare = list(formula = ~pctex21 + pctBrBnk) 
   Psi.pct21_shade = list(formula = ~pctex21 + pctShade)
-  Psi.pool_lwd = list(formula = ~pctpool + LWD) 
   Psi.pool_bare = list(formula = ~pctpool + pctBrBnk) 
   Psi.pool_shade = list(formula = ~pctpool + pctShade) 
-  Psi.rock_lwd = list(formula = ~pctrock + LWD) 
   Psi.rock_bare = list(formula = ~pctrock + pctBrBnk) 
   Psi.rock_shade = list(formula = ~pctrock + pctShade) 
-  Psi.BRT_lwd = list(formula = ~BRT_100m + LWD) 
   Psi.BRT_bare = list(formula = ~BRT_100m + pctBrBnk) 
   Psi.BRT_shade = list(formula = ~BRT_100m + pctShade) 
   #~~~~~~~~~~~~~ Occupancy - single covariate ~~~~~~~~~~~~~~~~~~~~~~
   Psi.pctex21 = list(formula=~pctex21) 
   Psi.pool = list(formula=~pctpool) 
-  Psi.MacroProp = list(formula=~machabprop) 
   Psi.rock = list(formula=~pctrock) 
   Psi.bare = list(formula=~pctBrBnk) 
   Psi.shade = list(formula=~pctShade) 
-  Psi.brt = list(formula=~BRT_100m) 
+  Psi.brt = list(formula=~BRT_100m)
+  Psi.forest = list(formula = ~HAiFLS_for)
   #~~~~~~~~~~~~ model list & wrapper ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  cml=create.model.list("Occupancy")
-  results=mark.wrapper(cml, data=brook.process, ddl=bkt.ddl, output=F)
-  return(results)
+  cml.loc=create.model.list("Occupancy")
+  results.loc=mark.wrapper(cml.loc, data=brook.process, ddl=bkt.ddl, output=F)
+  return(results.loc)
 }
 
-bkt.results = run.occ()
+bkt.results.loc = run.occ.loc()
 
 
 ##Examine model list and look at model comparisons
-bkt.results
+bkt.results.loc
 ##Model Table
-bkt.results$model.table = model.table(bkt.results, use.lnl = T)
-bkt.results$model.table
+AICc.Table.loc = model.table(bkt.results.loc, use.lnl = T)
+AICc.Table.loc
 
-#look at summary of top model
-summary(bkt.results$p.tv.effort.Psi.pct21_brt)
-names(bkt.results)
-round1.top <- bkt.results$p.tv.effort.Psi.pct21_brt
-summary(round1.top, showall = F)
-round1.top$results$real
+#look at summary of top model(s)
+summary(bkt.results.loc$p.tv.effort.Psi.pct21_brt)
+bkt.results.loc$p.tv.effort.Psi.pct21_brt$results$real
+top.local <- bkt.results.loc$p.tv.effort.Psi.pct21_brt 
 
-summary(bkt.results$p.tv.effort.Psi.biology)
-summary(bkt.results$p.tv.effort.Psi.global)
 
 
 cleanup(ask = FALSE)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
+
+
+
+###~~~~~~~~~~~~~~~~~~##
+####  Catchment   ####
+##~~~~~~~~~~~~~~~~~~##
+
+run.occ.cat=function()
+{
+  #~~~~~~~~~~~~~ Model List ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  #~~~~~~~~~~~ Detection Probability - null model ~~~~~~~~~~~~
+  p.Dot = list(formula= ~1)
+  #~~~~~~~~~~~ Detection Probability - single covariate ~~~~~~~~~~~~
+  p.tv.effort = list(formula = ~effort)
+  #~~~~~~~~~~~~~ Occupancy - null model ~~~~~~~~~~~~~~~~~~~~~~
+  Psi.Dot        = list(formula=~1) 
+  #~~~~~~~~~~~~~ Occupancy - multiple covariates ~~~~~~~~~~~~~~~~~~~~~~
+  #"global" catchment models
+  Psi.cat = list(formula = ~HAiFLS_for+Area_km2+AvgSlope+EFac_Cat+Cross_Cat)
+  #submodels
+  #4 covariates
+  Psi.for1 = list(formula = ~HAiFLS_for+Area_km2+AvgSlope+EFac_Cat)
+  Psi.for2 = list(formula = ~HAiFLS_for+Area_km2+AvgSlope+Cross_Cat)
+  Psi.for3 = list(formula = ~HAiFLS_for+Area_km2+EFac_Cat+Cross_Cat)
+  Psi.for4 = list(formula = ~HAiFLS_for+AvgSlope+EFac_Cat+Cross_Cat)
+  Psi.for5 = list(formula = ~Area_km2+AvgSlope+EFac_Cat+Cross_Cat)
+  #3 covariates
+  Psi.for6 = list(formula = ~HAiFLS_for+Area_km2+AvgSlope)
+  Psi.for7 = list(formula = ~HAiFLS_for+Area_km2+EFac_Cat)
+  Psi.for8 = list(formula = ~HAiFLS_for+Area_km2+Cross_Cat)
+  Psi.for9 = list(formula = ~HAiFLS_for+AvgSlope+EFac_Cat)
+  Psi.for10 = list(formula = ~HAiFLS_for+AvgSlope+Cross_Cat)
+  Psi.for11 = list(formula = ~Area_km2+AvgSlope+EFac_Cat)
+  Psi.for12 = list(formula = ~Area_km2+AvgSlope+Cross_Cat)
+  Psi.for13 = list(formula = ~AvgSlope+EFac_Cat+Cross_Cat)
+  #2 covariates
+  Psi.for_area = list(formula = ~HAiFLS_for+Area_km2)
+  Psi.for_slope = list(formula = ~HAiFLS_for+AvgSlope)
+  Psi.for_EFac = list(formula = ~HAiFLS_for+EFac_Cat)
+  Psi.for_cross = list(formula = ~HAiFLS_for+Cross_Cat)
+  Psi.area_slope = list(formula = ~Area_km2+AvgSlope)
+  Psi.area_EFac = list(formula = ~Area_km2+EFac_Cat)
+  Psi.area_cross = list(formula = ~Area_km2+Cross_Cat)
+  Psi.slope_EFac = list(formula = ~AvgSlope+EFac_Cat)
+  Psi.slope_cross = list(formula = ~AvgSlope+Cross_Cat)
+  Psi.EFac_cross = list(formula = ~EFac_Cat+Cross_Cat)
+  #~~~~~~~~~~~~~ Occupancy - single covariate ~~~~~~~~~~~~~~~~~~~~~~
+  Psi.for = list(formula = ~HAiFLS_for)
+  Psi.area = list(formula = ~Area_km2)
+  Psi.slope = list(formula = ~AvgSlope)
+  Psi.EFac = list(formula = ~EFac_Cat)
+  Psi.cross = list(formula = ~Cross_Cat)
+  #~~~~~~~~~~~~ model list & wrapper ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  cml.cat=create.model.list("Occupancy")
+  results.cat=mark.wrapper(cml.cat, data=brook.process, ddl=bkt.ddl, output=F)
+  return(results.cat)
+}
+
+bkt.results.cat = run.occ.cat()
+
+##Examine model list and look at model comparisons
+bkt.results.cat
+##Model Table
+AICc.Table.Cat = model.table(bkt.results.cat, use.lnl = T)
+AICc.Table.Cat
+
+#look at summary of top model(s)
+summary(bkt.results.cat$p.tv.effort.Psi.for)
+bkt.results.cat$p.tv.effort.Psi.for$results$real
+
+
+cleanup(ask = FALSE)
 
 #---------------------------------------------------------------------------------------------------#
 #export MARK data with models
