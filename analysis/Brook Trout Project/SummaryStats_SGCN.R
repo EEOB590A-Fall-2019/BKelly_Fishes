9a54cb8eaeb44116cf15d3cd3b947bd04be6a321
bbkelly95 <bbkelly@iastate.edu>
2020-02-17 11:59:11 -0600

    SGCN-BRT analysis

--- analysis/Brook Trout Project/SummaryStats_SGCN.R
+++ analysis/Brook Trout Project/SummaryStats_SGCN.R
@@ -328,16 +328,87 @@ ggplot(data = cott, aes(x=BRT,y=Cottus_CPUE)) +
 
 #############################################################################
 
+# Mann Whitney U / Wilcox Sign Rank Test 
 
+# using subsetted data -- only when SGCNs of interest are present
 
+help("wilcox.test")
 
+# Ho: Median CPUE of SGCN when BRT are present = CPUE when BRT are absent
+# two-sided
 
+#-----
+#LND
+#-----
+class(ldace$BRT)
+wilcox.test(ldace$LND_CPUE ~ ldace$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# no difference
 
+#-----
+#SRD
+#-----
+class(sdace$BRT)
+wilcox.test(sdace$SRD_CPUE ~ sdace$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+#no difference
+#-----
+#Cottus
+#-----
+class(cott$BRT)
+wilcox.test(cott$Cottus_CPUE ~ cott$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+#no difference
 
+#############################################################################
 
+# Using the "coin" package
+library(coin)
+# Exact Wilcoxon Mann Whitney Rank Sum Test
+# where y is numeric and A is a binary factor
 
+#LND
+wilcox_test(LND_CPUE~BRT, data=ldace, distribution="exact") #p = 0.95
+#SRD
+wilcox_test(SRD_CPUE~BRT, data=sdace, distribution="exact") #p = 0.25
+#Cottus
+wilcox_test(Cottus_CPUE~BRT, data=cott, distribution="exact") #p = 1
 
+# One-Way Permutation Test based on 9999 Monte-Carlo
+# resamplings. y is numeric and A is a categorical factor
 
+#LND
+oneway_test(LND_CPUE~BRT, data=ldace,
+            distribution=approximate(B=9999)) #p = 0.95
+#SRD
+oneway_test(SRD_CPUE~BRT, data=sdace,
+            distribution=approximate(B=9999)) #p = 0.16
+#Cottus
+oneway_test(Cottus_CPUE~BRT, data=cott,
+            distribution=approximate(B=9999)) #p = 0.91
+#############################################################################
+
+# Results:
+  # Longnose Dace:
+    # occurr = 33
+    # sympatry = 19, allopatry = 14
+    # no significant difference in CPUE when BRT present vs. absent
+  # Southern Redbelly Dace:
+    # occurr = 33
+    # sympatry = 12, allopatry = 21
+    # no significant difference in CPUE when BRT present vs. absent
+  # Sculpins:
+    # occurr = 20
+    # sympatry = 18, allopatry = 2
+    # no significant difference in CPUE when BRT present vs. absent
+
+#############################################################################
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Let's build some CPUE models for each non-game fish with a combination of 
+# hypotheses: 1) only environment, 2) Brown Trout, 3) All "carnivores", 
+# 4) BRT+environment, 5) TC+environment, 6) null
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 
 
