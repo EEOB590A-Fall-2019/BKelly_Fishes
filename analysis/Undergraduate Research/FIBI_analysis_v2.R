8945718b6c8bf2dac3240a330eebc5954226c64c
bbkelly95 <bbkelly@iastate.edu>
2020-02-24 12:00:53 -0600

    FIBI code

--- analysis/Undergraduate Research/FIBI_analysis_v2.R
+++ analysis/Undergraduate Research/FIBI_analysis_v2.R
@@ -4,12 +4,15 @@ library(car)
 library(MASS)
 library(ggResidpanel)
 library(cowplot)
+library(pracma)
+library(tidyverse)
 
 ### Massage the dataset
 mydat <- read.csv("Data/Thesis/Tidy/FIBI_tidy2.csv", header = T)
 mydat$Rating <- factor(mydat$Rating, levels = c("Very Poor", "Poor", "Fair", "Good", "Excellent"))
 mydat$Year <- factor(mydat$Year, levels = c("2018", "2019"))
 mydat$HUC8 <- as.factor(mydat$HUC8)
+
 
 mydat$HUC10 <- as.factor(sub('.*(?=.{3}$)', '', mydat$HUC_10, perl=T))
 mydat$HUC12 <- as.factor(sub('.*(?=.{2}$)', '', mydat$HUC_12, perl=T))
@@ -53,6 +56,65 @@ Anova(mod_norm2, type = "III")
 
 #Use predict function to plot significant covariates
 mydat$predict <- predict(mod_norm2, type = "response")  
+
+######################################################
+#predict while holding other values constant 
+#####################################################
+huc.mean <- as.factor(rep("YEL", 50)) #HUC8=YEL
+huc.UPI <- as.factor(rep("UPI", 50)) #HUC8=UPI
+huc.LMAQ <- as.factor(rep("LMAQ", 50)) #HUC8=LMAQ
+temp.mean <- rep(mean(mydat$MEANT), 50) #MEANT
+run.mean <- rep(mean(mydat$pctrun), 50) #pctrun
+rock.mean <- rep(mean(mydat$pctrock), 50) #pctrock
+shade.mean <- rep(mean(mydat$pctShade), 50) #pctShade
+bare.mean <- rep(mean(mydat$pctBrBnk), 50) #pctBrBnk
+dev.mean <- rep(mean(mydat$HAiFLS_dev), 50) #HAiFLS_dev
+for.mean <- rep(mean(mydat$HAiFLS_for), 50) #HAiFLS_for
+
+
+#-----
+# Vector of levels of random grouping variable - HUC_12
+#-----
+basin <- factor(levels(mydat$HUC8))
+basin
+watersheds <- levels(mydat$watershed_sm)
+watersheds
+
+#------------------------------------------
+#Dataframes for each predictor of interest
+#------------------------------------------
+
+#MEANT
+min.temp <- min(mydat$MEANT)
+max.temp <- max(mydat$MEANT)
+temp.values <- seq(from = min.temp, to = max.temp, length = 50)
+temperature <- data.frame(HUC8 = huc.mean, MEANT = temp.values, pctrun = run.mean,
+                          pctrock = rock.mean, pctShade = shade.mean, pctBrBnk = bare.mean,
+                          HAiFLS_dev = dev.mean, HAiFLS_for = for.mean)
+temp.df <- crossing(temperature, watersheds)
+
+#pctShade
+min.shade <- min(mydat$pctShade)
+max.shade <- max(mydat$pctShade)
+shade.values <- seq(from = min.shade, to = max.shade, length = 50)
+canopy <- data.frame(HUC8 = huc.mean, MEANT = temp.mean, pctrun = run.mean,
+                          pctrock = rock.mean, pctShade = shade.values, pctBrBnk = bare.mean,
+                          HAiFLS_dev = dev.mean, HAiFLS_for = for.mean)
+canopy.df <- crossing(canopy, watersheds)
+
+#pctBrBnk
+min.bare <- min(mydat$pctBrBnk)
+max.bare <- max(mydat$pctBrBnk)
+bare.values <- seq(from = min.bare, to = max.bare, length = 50)
+barebank <- data.frame(HUC8 = huc.mean, MEANT = temp.mean, pctrun = run.mean,
+                     pctrock = rock.mean, pctShade = shade.mean, pctBrBnk = bare.values,
+                     HAiFLS_dev = dev.mean, HAiFLS_for = for.mean)
+bare.df <- crossing(barebank, watersheds)
+
+
+
+
+
 
 #observed vs. predicted
 ggplot(mydat, aes(x = IBIScore, y = predict)) + geom_point()+
