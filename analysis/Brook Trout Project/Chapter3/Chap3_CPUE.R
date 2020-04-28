d889408f9c8335d63fe86661f81484222ea2f233
bbkelly95 <bbkelly@iastate.edu>
2020-04-28 13:48:22 -0500

    chapter 3 - cpue analyses

--- analysis/Brook Trout Project/Chapter3/Chap3_CPUE.R
+++ analysis/Brook Trout Project/Chapter3/Chap3_CPUE.R
@@ -22,7 +22,7 @@ library(coin)
 #load data
 newdat <- read.csv("Data/Thesis/Tidy/chpt3_tidy.csv", header=T)
 mydat <- read.csv("Data/Thesis/Tidy/SGCN_AllCovariates.csv", header=T)
-
+all <- left_join(newdat, mydat, by="newID")
 #inspect
 skim(mydat)
 
@@ -103,6 +103,197 @@ x.grob <- textGrob("Brown Trout Status (Presence/Absence)",
 bf2 <- grid.arrange(arrangeGrob(boxplot.figure, bottom = x.grob))
 bf2
 
-ggsave("Figure_3.png", plot = bf2, dpi = 600)
+#ggsave("Figure_3.png", plot = bf2, dpi = 600)
 
+
+
+
+
+
+#############################################################################
+
+# Mann Whitney U / Wilcox Sign Rank Test 
+
+# using subsetted data -- only when SGCNs of interest are present
+
+help("wilcox.test")
+
+# Ho: Median CPUE of SGCN when BRT are present = CPUE when BRT are absent
+# two-sided
+
+#-----
+#LND
+#-----
+class(ldace$BRT)
+wilcox.test(ldace$LND_CPUE ~ ldace$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# no difference
+
+#-----
+#SRD
+#-----
+class(sdace$BRT)
+wilcox.test(sdace$SRD_CPUE ~ sdace$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+#no difference
+#-----
+#Cottus
+#-----
+class(cott$BRT)
+wilcox.test(cott$Cottus_CPUE ~ cott$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+#no difference
+
+#############################################################################
+
+# Using the "coin" package
+# Exact Wilcoxon Mann Whitney Rank Sum Test
+# where y is numeric and A is a binary factor
+
+#LND
+wilcox_test(LND_CPUE~BRT, data=ldace, distribution="exact") #p = 0.95
+#SRD
+wilcox_test(SRD_CPUE~BRT, data=sdace, distribution="exact") #p = 0.25
+#Cottus
+wilcox_test(Cottus_CPUE~BRT, data=cott, distribution="exact") #p = 1
+
+# One-Way Permutation Test based on 9999 Monte-Carlo
+# resamplings. y is numeric and A is a categorical factor
+
+#LND
+oneway_test(LND_CPUE~BRT, data=ldace,
+            distribution=approximate(B=9999)) #p = 0.95
+#SRD
+oneway_test(SRD_CPUE~BRT, data=sdace,
+            distribution=approximate(B=9999)) #p = 0.16
+#Cottus
+oneway_test(Cottus_CPUE~BRT, data=cott,
+            distribution=approximate(B=9999)) #p = 0.91
+#############################################################################
+
+# Results:
+# Longnose Dace:
+# occurr = 33
+# sympatry = 19, allopatry = 14
+# no significant difference in CPUE when BRT present vs. absent
+# Southern Redbelly Dace:
+# occurr = 33
+# sympatry = 12, allopatry = 21
+# no significant difference in CPUE when BRT present vs. absent
+# Sculpins:
+# occurr = 20
+# sympatry = 18, allopatry = 2
+# no significant difference in CPUE when BRT present vs. absent
+
+#############################################################################
+
+
+
+
+
+#############################################################################
+
+#---------------------------------CPUE models--------------------------------
+
+#############################################################################
+#libraries
+library(pscl)
+#library(MASS)
+library(boot)
+
+#explore data
+names(newdat)
+
+
+#inspect response variable(s)
+ggplot(newdat, aes(LND_CPUE))+
+  geom_histogram(binwidth = 10) #Longnose Dace (CPUE)
+
+ggplot(newdat, aes(SRD_CPUE))+
+  geom_histogram(binwidth = 10) #Southern Redbelly Dace CPUE
+
+ggplot(newdat, aes(Cottus_CPUE))+
+  geom_histogram(binwidth = 10) #Sculpins CPUE
+
+#inspect predictor variables
+predictors <- newdat %>%
+  select(avwid, pctfines, pctriffle, BrBank, MEANT, HAiFLS_alt, HAiFLS_for, pctSlope, avgT, mFlow, 
+         adult_100m, med_len, mean_len)
+
+ggplot(predictors, aes(avwid))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(pctfines))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(pctriffle))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(BrBank))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(MEANT))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(HAiFLS_alt))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(HAiFLS_for))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(pctSlope))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(avgT))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(mFlow))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(adult_100m))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(med_len))+
+  geom_histogram(binwidth = 1)
+ggplot(predictors, aes(mean_len))+
+  geom_histogram(binwidth = 1)
+
+#############################################################################
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Let's build some CPUE models for each non-game fish with a combination of 
+# hypotheses: 1) full model, 2) environment only, 3) BRT variables only, 
+# 4) null model
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+# Longnose Dace
+# Intermediate tolerance, benthic, not TC, native eurythermal
+# Hypotheses:
+# Environment: %cobble(+), avwidth(+), pctSlope(-)
+# BRT influence: (~) Habitat overlap seemingly with BRT, benthic sp. 
+# maybe less susceptible to predation 
+
+# Southern Redbelly Dace
+# Intermediate tolerance, column, not TC, native eurythermal
+# Hypotheses:
+# Environment: MEANT(+), HAiFLS_alt(-), avdep(+)
+# BRT influence: (-) column dwelling cyprinid - seems
+# susceptible to predation
+
+#Sculpins
+# Intolerant, not TC, native coldwater, benthic
+# Hypotheses:
+# Environment: avgT(-), BrBank(-), HAiFLS_for(+), mFlow(-)
+# BRT influence: (+) habitat overlap, BRT invade -- abundance more
+# of a function of habitat since intolerant, and coldwater
+
+#############################################################################
+require(pscl)
+require(MASS)
+require(boot)
+
+
+names(all)
+
+#Longnose Dace all additive global model
+#zero-inflated negative binomial model 
+
+lnd.mod <- zeroinfl(LND_ab ~ avwid.x+pctcbbl+pctSlope.x+med_len+adult_100m | avwid.x+adult_100m,
+               data = all, dist = "negbin", EM = TRUE)
+summary(lnd.mod)
+
+#lnd.mod <- mixed_model(fixed = LND_CPUE ~ avwid.x+pctcbbl+pctSlope.x+med_len+adult_100m,
+                    #data = all,
+                    #family = zi.negative.binomial(), zi_fixed = ~ avwid.x+adult_100m,
+                    #iter_EM=0)
+#summary(lnd.mod)
 
