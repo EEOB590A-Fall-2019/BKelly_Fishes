5f0064115409040f80bf4b1acbd7caf80dabdb6d
bbkelly95 <bbkelly@iastate.edu>
2020-04-30 11:47:55 -0500

    size comparison and new Figure 5

--- analysis/Brook Trout Project/Chapter3/Chapt_Three_SizeComp.R
+++ analysis/Brook Trout Project/Chapter3/Chapt_Three_SizeComp.R
@@ -26,6 +26,7 @@ cott.list <- vars_select(nms, ends_with("Cottus"))
 lnd.brt <- sizes %>%
   select(BRT, lnd.list)
 
+
 lnd.b.long <- lnd.brt %>%
   rename(bin1 = bin1_LND, bin2 = bin2_LND, bin3 = bin3_LND) %>%
   pivot_longer(
@@ -58,6 +59,46 @@ a
 
 #lnd.ad <- sizes %>%
 #  select(adult_status, lnd.list)
+
+lnd_sums <- sizes2 %>%
+  select(newID, BRT, adult_status, lnd.list) %>%
+  mutate(Sum = bin1_LND+bin2_LND+bin3_LND) %>%
+  mutate(P_A = ifelse(Sum>0,1,0)) %>%
+  mutate_at("P_A", as.factor) %>%
+  filter(P_A == 1)%>%
+  rename(bin1 = bin1_LND, bin2 = bin2_LND, bin3 = bin3_LND) %>%
+  pivot_longer(
+    cols = starts_with("bin"),
+    names_to = "Bin",
+    names_prefix = "bin",
+    values_to = "count"
+  ) %>%
+  mutate_if(is.character, as.factor) %>%
+  mutate_at("BRT", as.factor) %>%
+  group_by(BRT, Bin) %>%
+  summarise(Count = sum(count), Mean_Count = mean(count), SD = sd(count), Med = median(count)) %>%
+  mutate(SE = (SD/sqrt(33)))
+
+a2 <- ggplot(lnd_sums, aes(fill=BRT, y=Mean_Count, x=Bin)) + 
+  geom_bar(position="dodge", stat="identity", colour = "black") +
+  geom_errorbar(aes(ymin=Mean_Count, ymax=Mean_Count+SE), width=.2,
+                position=position_dodge(.9))+
+  theme_bw() +
+  theme(panel.grid = element_blank())+
+  labs(y=NULL, x=NULL)+
+  scale_x_discrete(labels = c("1" = "30-60", "2" = "60-90", "3" = "90-120"))+
+  scale_fill_manual(values = c("white", "black"),
+                    labels = c("0" = "No Brown Trout", "1" = "Brown Trout"))+
+  theme(axis.title = element_text(size = 12))+
+  ggtitle("(a)")+
+  theme(plot.title = element_text(size=14, face = "bold"))+
+  theme(legend.position = c(0.85,0.80))+
+  theme(legend.title = element_blank())+
+  annotate("text", x = 0.75, y = 22, label = "Longnose Dace")
+a2
+
+
+
 #-----------------------------------
 
 
@@ -100,6 +141,42 @@ b
 
 #cott.ad <- sizes %>%
 #  select(adult_status, cott.list)
+
+cott_sums <- sizes2 %>%
+  select(newID, BRT, adult_status, cott.list) %>%
+  mutate(Sum = bin1_Cottus+bin2_Cottus+bin3_Cottus+bin4_Cottus) %>%
+  mutate(P_A = ifelse(Sum>0,1,0)) %>%
+  mutate_at("P_A", as.factor) %>%
+  filter(P_A == 1)%>%
+  rename(bin1 = bin1_Cottus, bin2 = bin2_Cottus, bin3 = bin3_Cottus, bin4 = bin4_Cottus) %>%
+  pivot_longer(
+    cols = starts_with("bin"),
+    names_to = "Bin",
+    names_prefix = "bin",
+    values_to = "count"
+  ) %>%
+  mutate_if(is.character, as.factor) %>%
+  mutate_at("BRT", as.factor) %>%
+  group_by(BRT, Bin) %>%
+  summarise(Count = sum(count), Mean_Count = mean(count), SD = sd(count), Med = median(count)) %>%
+  mutate(SE = (SD/sqrt(33)))
+
+b2 <- ggplot(cott_sums, aes(fill=BRT, y=Mean_Count, x=Bin)) + 
+  geom_bar(position="dodge", stat="identity", colour = "black") +
+  geom_errorbar(aes(ymin=Mean_Count, ymax=Mean_Count+SE), width=.2,
+                position=position_dodge(.9))+
+  theme_bw() +
+  theme(panel.grid = element_blank())+
+  labs(y=NULL, x=NULL)+
+  scale_x_discrete(labels = c("1" = "30-60", "2" = "60-90", "3" = "90-120", "4" = "120-150"))+
+  scale_fill_manual(values = c("white", "black"))+
+  theme(axis.title = element_text(size = 12))+
+  ggtitle("(b)")+
+  theme(plot.title = element_text(size=14, face = "bold"))+
+  theme(legend.position = "none")+
+  theme(legend.title = element_blank())+
+  annotate("text", x = 0.65, y = 70, label = "Sculpins")
+b2
 #-----------------------------------
 
 
@@ -139,17 +216,54 @@ c <- ggplot(srd.b.long, aes(fill=BRT, y=Count, x=Bin)) +
                      labels = c("0","100","200","300","400"))
 c
 
-#library(cowplot)
+library(cowplot)
 combo <- plot_grid(a,b,c, ncol=1)
 
 #srd.ad <- sizes %>%
 #  select(adult_status, srd.list)
+srd_sums <- sizes2 %>%
+  select(newID, BRT, adult_status, srd.list) %>%
+  mutate(Sum = bin1_SRD+bin2_SRD+bin3_SRD) %>%
+  mutate(P_A = ifelse(Sum>0,1,0)) %>%
+  mutate_at("P_A", as.factor) %>%
+  filter(P_A == 1)%>%
+  rename(bin1 = bin1_SRD, bin2 = bin2_SRD, bin3 = bin3_SRD) %>%
+  pivot_longer(
+    cols = starts_with("bin"),
+    names_to = "Bin",
+    names_prefix = "bin",
+    values_to = "count"
+  ) %>%
+  mutate_if(is.character, as.factor) %>%
+  mutate_at("BRT", as.factor) %>%
+  group_by(BRT, Bin) %>%
+  summarise(Count = sum(count), Mean_Count = mean(count), SD = sd(count), Med = median(count)) %>%
+  mutate(SE = (SD/sqrt(33)))
+
+c2 <- ggplot(srd_sums, aes(fill=BRT, y=Mean_Count, x=Bin)) + 
+  geom_bar(position="dodge", stat="identity", colour = "black") +
+  geom_errorbar(aes(ymin=Mean_Count, ymax=Mean_Count+SE), width=.2,
+                position=position_dodge(.9))+
+  theme_bw() +
+  theme(panel.grid = element_blank())+
+  labs(y=NULL, x=NULL)+
+  scale_x_discrete(labels = c("1" = "30-60", "2" = "60-90", "3" = "90-120", "4" = "120-150"))+
+  scale_fill_manual(values = c("white", "black"))+
+  theme(axis.title = element_text(size = 12))+
+  ggtitle("(b)")+
+  theme(plot.title = element_text(size=14, face = "bold"))+
+  theme(legend.position = "none")+
+  theme(legend.title = element_blank())+
+  annotate("text", x = 0.9, y = 25, label = "Southern Redbelly Dace")
+c2
+
+combo2 <- plot_grid(a2,b2,c2, ncol=1)
 #-----------------------------------
 
 #create common x axis label
 library(gridExtra)
 library(grid)
-y.grob <- textGrob("Count", 
+y.grob <- textGrob("Number of Fish", 
                    gp=gpar(col="black", fontsize=14),
                    rot = 90)
 x.grob <- textGrob("Size Class (mm)", 
@@ -157,8 +271,244 @@ x.grob <- textGrob("Size Class (mm)",
 #add to plot
 fig_5 <- grid.arrange(arrangeGrob(combo, left = y.grob, bottom = x.grob))
 fig_5
-ggsave("Figure_5.png", plot = fig_5, dpi = 600)
 
+fig_5.2 <- grid.arrange(arrangeGrob(combo2, left = y.grob, bottom = x.grob))
+fig_5.2
+ggsave("Figure_5.png", plot = fig_5.2, dpi = 600)
+#-----------------------------------
+
+
+#---------------
+# Comparisons
+#---------------
+
+str(sizes)
+
+sizes2 <- sizes %>%
+  mutate_at(c("BRT","adult_status"), as.factor)
+
+
+#############################################################################
+
+# Mann Whitney U / Wilcox Sign Rank Test 
+
+# using subsetted data -- only when SGCNs of interest are present
+
+help("wilcox.test")
+
+# Ho: Median CPUE of SGCN when BRT are present = CPUE when BRT are absent
+# two-sided
+
+#-----
+#LND
+#-----
+lnd.comp <- sizes2 %>%
+  select(newID, BRT, adult_status, lnd.list) %>%
+  mutate(Sum = bin1_LND+bin2_LND+bin3_LND) %>%
+  mutate(P_A = ifelse(Sum>0,1,0)) %>%
+  mutate_at("P_A", as.factor) %>%
+  filter(P_A == 1)
+
+## compare across BRT status
+#size class 1
+wilcox.test(lnd.comp$bin1_LND ~ lnd.comp$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.3945
+
+#size class 2
+wilcox.test(lnd.comp$bin2_LND ~ lnd.comp$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.6726
+
+#size class 3
+wilcox.test(lnd.comp$bin3_LND ~ lnd.comp$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.6321
+
+#-----
+## compare across adult proportion status
+#size class 1
+wilcox.test(lnd.comp$bin1_LND ~ lnd.comp$adult_status, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.3081
+
+#size class 2
+wilcox.test(lnd.comp$bin2_LND ~ lnd.comp$adult_status, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.7436
+
+#size class 3
+wilcox.test(lnd.comp$bin3_LND ~ lnd.comp$adult_status, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.7078
+
+
+
+
+#-----
+#SRD
+#-----
+srd.comp <- sizes2 %>%
+  select(newID, BRT, adult_status, srd.list) %>%
+  mutate(Sum = bin1_SRD+bin2_SRD+bin3_SRD) %>%
+  mutate(P_A = ifelse(Sum>0,1,0)) %>%
+  mutate_at("P_A", as.factor) %>%
+  filter(P_A == 1)
+
+## compare across BRT status
+#size class 1
+wilcox.test(srd.comp$bin1_SRD ~ srd.comp$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.1305
+
+#size class 2
+wilcox.test(srd.comp$bin2_SRD ~ srd.comp$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.6886
+
+#size class 3
+wilcox.test(srd.comp$bin3_SRD ~ srd.comp$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 1
+
+#-----
+## compare across adult proportion status
+#size class 1
+wilcox.test(srd.comp$bin1_SRD ~ srd.comp$adult_status, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.6122
+
+#size class 2
+wilcox.test(srd.comp$bin2_SRD ~ srd.comp$adult_status, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.3798
+
+#size class 3
+wilcox.test(srd.comp$bin3_SRD ~ srd.comp$adult_status, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.6737
+
+
+
+#-----
+#Cottus
+#-----
+cott.comp <- sizes2 %>%
+  select(newID, BRT, adult_status, cott.list) %>%
+  mutate(Sum = bin1_Cottus+bin2_Cottus+bin3_Cottus+bin4_Cottus) %>%
+  mutate(P_A = ifelse(Sum>0,1,0)) %>%
+  mutate_at("P_A", as.factor) %>%
+  filter(P_A == 1)
+
+## compare across BRT status
+#size class 1
+wilcox.test(cott.comp$bin1_Cottus ~ cott.comp$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.7411
+
+#size class 2
+wilcox.test(cott.comp$bin2_Cottus ~ cott.comp$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.6579
+
+#size class 3
+wilcox.test(cott.comp$bin3_Cottus ~ cott.comp$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.7484
+
+#size class 4
+wilcox.test(cott.comp$bin4_Cottus ~ cott.comp$BRT, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.611
+
+#-----
+## compare across adult proportion status
+#size class 1
+wilcox.test(cott.comp$bin1_Cottus ~ cott.comp$adult_status, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.5448
+
+#size class 2
+wilcox.test(cott.comp$bin2_Cottus ~ cott.comp$adult_status, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.967
+
+#size class 3
+wilcox.test(cott.comp$bin3_Cottus ~ cott.comp$adult_status, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.801
+
+#size class 4
+wilcox.test(cott.comp$bin4_Cottus ~ cott.comp$adult_status, mu=0, alt="two.sided", conf.int=T, conf.level=0.95, paired=F,
+            exact=F)
+# p-value = 0.9469
+
+#############################################################################
+
+# Using the "coin" package
+# Exact Wilcoxon Mann Whitney Rank Sum Test
+# where y is numeric and A is a binary factor
+
+#LND
+wilcox_test(bin1_LND~BRT, data=lnd.comp, distribution="exact") #p-value = 0.4046
+wilcox_test(bin2_LND~BRT, data=lnd.comp, distribution="exact") #p-value = 0.6701
+wilcox_test(bin3_LND~BRT, data=lnd.comp, distribution="exact") #p-value = 0.6269
+
+#SRD
+wilcox_test(bin1_SRD~BRT, data=srd.comp, distribution="exact") #p-value = 0.1284
+wilcox_test(bin2_SRD~BRT, data=srd.comp, distribution="exact") #p-value = 0.6859
+wilcox_test(bin3_SRD~BRT, data=srd.comp, distribution="exact") #p-value = 1
+
+#Cottus
+wilcox_test(bin1_Cottus~BRT, data=cott.comp, distribution="exact") #p-value = 0.7368
+wilcox_test(bin2_Cottus~BRT, data=cott.comp, distribution="exact") #p-value = 0.6526
+wilcox_test(bin3_Cottus~BRT, data=cott.comp, distribution="exact") #p-value = 0.7368
+wilcox_test(bin4_Cottus~BRT, data=cott.comp, distribution="exact") #p-value = 1
+
+# One-Way Permutation Test based on 9999 Monte-Carlo
+# resamplings. y is numeric and A is a categorical factor
+
+#LND
+oneway_test(bin1_LND~BRT, data=lnd.comp,
+            distribution=approximate(B=9999)) #p = 0.1881
+oneway_test(bin2_LND~BRT, data=lnd.comp,
+            distribution=approximate(B=9999)) #p = 0.9501
+oneway_test(bin3_LND~BRT, data=lnd.comp,
+            distribution=approximate(B=9999)) #p = 0.5884
+
+#SRD
+oneway_test(bin1_SRD~BRT, data=srd.comp,
+            distribution=approximate(B=9999)) #p = 0.1119
+oneway_test(bin2_SRD~BRT, data=srd.comp,
+            distribution=approximate(B=9999)) #p = 0.3327
+oneway_test(bin3_SRD~BRT, data=srd.comp,
+            distribution=approximate(B=9999)) #p = 0.7605
+#Cottus
+oneway_test(bin1_Cottus~BRT, data=cott.comp,
+            distribution=approximate(B=9999)) #p = 0.6017
+oneway_test(bin2_Cottus~BRT, data=cott.comp,
+            distribution=approximate(B=9999)) #p = 0.7618
+oneway_test(bin3_Cottus~BRT, data=cott.comp,
+            distribution=approximate(B=9999)) #p = 0.8168
+oneway_test(bin4_Cottus~BRT, data=cott.comp,
+            distribution=approximate(B=9999)) #p = 1
+#############################################################################
+
+# Results:
+# Longnose Dace:
+# occurr = 33
+# sympatry = 19, allopatry = 14
+# no significant difference in size when BRT present vs. absent
+# Southern Redbelly Dace:
+# occurr = 33
+# sympatry = 12, allopatry = 21
+# no significant difference in size when BRT present vs. absent
+# Sculpins:
+# occurr = 20
+# sympatry = 18, allopatry = 2
+# no significant difference in size when BRT present vs. absent
+
+#############################################################################
 
 
 
